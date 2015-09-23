e82568a03b4de39ecb58b1f3c1751e8d256411d3
ArnoldPanuelo <arnopanu@comfsm.fm>
2015-09-23 16:39:30 +1100

    Create MiniProject1-Castro.cpp

---
+++ MiniProject1-Castro.cpp
@@ -0,0 +1,12 @@
+// Submitted By: Arnold Panuelo
+// Submitted To: Edper M. Castro
+// Mini Project 1
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!";
+}
+
